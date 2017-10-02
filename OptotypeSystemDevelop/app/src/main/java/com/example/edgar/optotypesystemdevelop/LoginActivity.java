a3ee4a0425ff1d3fdb1837afd766bb969e00d460
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-10-01 20:06:25 -0430

    Se reconstruyen cambios para ver e iniciar interaccion sobre ventana de login

--- OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/LoginActivity.java
+++ OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/LoginActivity.java
@@ -1,13 +1,49 @@
 package com.example.edgar.optotypesystemdevelop;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextUserName;
+    EditText    editTextPaswword;
+    Button buttonLogin;
+    ImageView imageViewIcon;
+    Context contextActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        contextActivity = this;
+
+        editTextUserName = (EditText) findViewById(R.id.editTextUserNameLogin);
+        editTextPaswword = (EditText) findViewById(R.id.editTextViewPassWord);
+        imageViewIcon   = (ImageView) findViewById(R.id.imageViewIconEye);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener((View.OnClickListener) contextActivity);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        callNewActivity();
+
+    }
+
+    public void callNewActivity (){
+
+        Intent dashBoardActivity = new Intent(this, DashBoardActivity.class);
+        startActivity(dashBoardActivity);
+
     }
 }
