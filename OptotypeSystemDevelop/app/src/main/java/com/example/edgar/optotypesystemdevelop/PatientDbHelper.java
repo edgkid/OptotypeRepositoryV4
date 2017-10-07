a9a3aaf5ec3834805edb246c8455c200720ea0b2
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-10-07 15:09:04 -0430

    Helper de base de datos para par apacientes

---
+++ OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/PatientDbHelper.java
@@ -0,0 +1,67 @@
+package com.example.edgar.optotypesystemdevelop;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Edgar on 07/10/2017.
+ */
+
+public class PatientDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "OpticalDataBaseApp";
+
+    public PatientDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION );
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql ="";
+        ContentValues values = new ContentValues();
+
+        // crear Tablas de base de datos
+        sql = sql + "CREATE TABLE " + PatientDbContract.PatientEntry.TABLE_NAME + " ( ";
+        sql = sql + PatientDbContract.PatientEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, ";
+        sql = sql + PatientDbContract.PatientEntry.ID + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.NAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.LASTNAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.MIDDLENAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.MAIDENNAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.YEARSOLD + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.PHOTO + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.FKUSER + " TEXT, ";
+        sql = sql + " UNIQUE ( " + PatientDbContract.PatientEntry.ID + " ) ";
+        sql = sql +" ) ";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String sql ="";
+        ContentValues values = new ContentValues();
+
+        // crear Tablas de base de datos
+        sql = sql + "CREATE TABLE " + PatientDbContract.PatientEntry.TABLE_NAME + " ( ";
+        sql = sql + PatientDbContract.PatientEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, ";
+        sql = sql + PatientDbContract.PatientEntry.ID + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.NAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.LASTNAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.MIDDLENAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.MAIDENNAME + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.YEARSOLD + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.PHOTO + " TEXT, ";
+        sql = sql + PatientDbContract.PatientEntry.FKUSER + " TEXT, ";
+        sql = sql + " UNIQUE ( " + PatientDbContract.PatientEntry.ID + " ) ";
+        sql = sql +" ) ";
+
+        db.execSQL(sql);
+    }
+}
