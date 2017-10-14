b55ad83b79d6b7397f591fa168d4b37a77eb58f3
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-10-14 07:50:53 -0430

    Modo de interacción con Drag and Drop implentado sobre el optotipo principal

--- OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/InteractionActivity.java
+++ OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/InteractionActivity.java
@@ -1,8 +1,14 @@
 package com.example.edgar.optotypesystemdevelop;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,8 +18,13 @@ public class InteractionActivity extends AppCompatActivity {
     TextView textLastNames;
     TextView textYearsOld;
 
+    ImageView imageOptotype;
     ImageView imagePerfil;
     ImageView imageEmotion;
+
+    ImageView imageOptotypeA;
+    ImageView imageOptotypeB;
+    ImageView imageOptotypeC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +34,18 @@ public class InteractionActivity extends AppCompatActivity {
         textNames = (TextView) findViewById(R.id.textViewNAmePatient);
         textLastNames = (TextView) findViewById(R.id.textViewLastnamePatient);
         textYearsOld = (TextView) findViewById(R.id.textYearsPatient);
+
+        imageOptotype = (ImageView) findViewById(R.id.imageViewOptotype);
         imageEmotion = (ImageView) findViewById(R.id.imageInteractionEmotion);
         imagePerfil = (ImageView) findViewById(R.id.imageViewInteractionPatient);
+
+        imageOptotypeA = (ImageView) findViewById(R.id.imageOptotypeOptionA);
+        imageOptotypeA = (ImageView) findViewById(R.id.imageOptotypeOptionB);
+        imageOptotypeA = (ImageView) findViewById(R.id.imageOptotypeOptionC);
+
+
+        imageOptotype.setOnLongClickListener(logClickListener);
+
 
         Intent intentData = getIntent();
         Bundle patientExtras = intentData.getExtras();
@@ -34,7 +55,48 @@ public class InteractionActivity extends AppCompatActivity {
             textLastNames.setText( (String) patientExtras.get("Patient"));
             textYearsOld.setText( (String) patientExtras.get("YearsOld") + " años");
         }
-
-
     }
+
+    View.OnLongClickListener  logClickListener = new View.OnLongClickListener() {
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public boolean onLongClick(View v) {
+
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+                v.startDragAndDrop(data,myShadowBuilder,v,0);
+            } else {
+                v.startDrag(data,myShadowBuilder,v,0);
+            }
+
+            return true;
+        }
+    };
+
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            int dragEvent = event.getAction();
+
+            Log.d("message: ", "Drag");
+            final View view = (View) event.getLocalState();
+            switch (dragEvent){
+                //Accion para mover elemento
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    break;
+
+
+            }
+            return true;
+        }
+    };
+
 }
