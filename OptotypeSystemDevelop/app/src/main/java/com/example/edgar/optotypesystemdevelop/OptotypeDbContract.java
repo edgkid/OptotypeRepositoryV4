0423f08db2874333a0f1ac8ce09ee72a3822ccd8
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-10-18 15:13:53 -0430

    clase de contrato para tabla de optotypos

---
+++ OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/OptotypeDbContract.java
@@ -0,0 +1,21 @@
+package com.example.edgar.optotypesystemdevelop;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Edgar on 18/10/2017.
+ */
+
+public class OptotypeDbContract {
+
+    public static abstract class OptotypeEntry implements BaseColumns{
+
+        public static final String TABLE_NAME = "optotype_db_app";
+
+        public static final String ID = "idOptotype";
+        public static final String OPTOTYPECODE = "optotypeCode";
+        public static final String OPTOTYPENAME = "optotypeName";
+
+    }
+
+}
