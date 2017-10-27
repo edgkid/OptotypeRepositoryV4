f4baf2d0f13bf0ffc3cc18f2657d1af322b54a21
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-10-27 09:57:55 -0430

    clase request para manipular datos de la iteracción

---
+++ OptotypeSystemDevelop/app/src/main/java/com/example/edgar/optotypesystemdevelop/RequestInteraction.java
@@ -0,0 +1,20 @@
+package com.example.edgar.optotypesystemdevelop;
+
+import android.content.Context;
+
+/**
+ * Created by Edgar on 27/10/2017.
+ */
+
+public class RequestInteraction {
+
+    private String request;
+    private Context context;
+
+    public RequestInteraction(String request, Context context) {
+        this.request = request;
+        this.context = context;
+    }
+    
+
+}
